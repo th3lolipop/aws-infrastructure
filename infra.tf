14cc65b34a7a715c4441aae5c586e1d2c4794523
Wai Yan Min <waiyanmin@yomafleet.com>
2020-09-22 00:45:12 +0630

    one more sg added

--- infra.tf
+++ infra.tf
@@ -62,6 +62,18 @@ module "web_server_sg" {
   ingress_cidr_blocks = var.sg.ingress
 }
 
+module "ssh_security_group" {
+
+  source  = "terraform-aws-modules/security-group/aws//modules/ssh"
+  version = "~> 3.0"
+
+  name        = var.sg.name
+  description = "Security group for SSH For Remote Execution"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress_cidr_blocks = var.sg.ingress
+}
+
 resource "aws_security_group" "allow_ssh" {
   name        = "Allow SSH"
   description = "Allow SSH inbound traffic from ANYWHERE"
@@ -84,5 +96,6 @@ resource "aws_security_group" "allow_ssh" {
   tags = {
     Terraform   = "true"
     Environment = "dev"
+    Name        = "SSH From Public"
   }
 }
