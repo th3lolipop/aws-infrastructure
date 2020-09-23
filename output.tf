525cb168b6c13fc446f426fee67164d633f57000
Wai Yan Min <waiyanmin@yomafleet.com>
2020-09-23 16:34:40 +0630

    rds endpoint output

--- output.tf
+++ output.tf
@@ -2,3 +2,8 @@ output "public_ip" {
   value       = module.ec2.public_ip
   description = "EC2 Instance Public IPv4 ADDRESS"
 }
+
+output "rds_endpoint" {
+  value       = module.rds.endpoint
+  description = "RDS Endpoint Name"
+}
