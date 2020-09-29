ba53f233ed187382483fffe0bbbe6e03d2cb62b1
Wai Yan Min <waiyanmin@yomafleet.com>
2020-09-29 11:15:18 +0630

    S3 Module added

---
+++ s3.tf
@@ -0,0 +1,15 @@
+module "s3-plus" {
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "1.12.0"
+
+  bucket        = var.s3_name
+  acl           = var.s3.is_private ? "private" : "public"
+  create_bucket = var.s3.bucket_create
+
+  tags = {
+    Environment = var.s3.env
+    Owner       = var.s3.owner
+    Project     = var.s3.project
+  }
+
+}
