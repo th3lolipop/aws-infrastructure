228fc3bca6d4645f784630843e5a8e79f19645e1
Wai Yan Min <waiyanmin@yomafleet.com>
2020-09-29 11:18:31 +0630

    output add

--- output.tf
+++ output.tf
@@ -7,3 +7,8 @@ output "rds_endpoint" {
   value       = module.rds.this_db_instance_endpoint
   description = "RDS Endpoint Name"
 }
+
+output "bucket_url" {
+  value       = module.s3-plus.this_s3_bucket_bucket_regional_domain_name
+  description = "AWS S3 Bucket URL with Region"
+}
